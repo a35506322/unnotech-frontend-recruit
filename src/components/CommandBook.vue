92c1e825bb3fe405bc081d969fc0ea8f035cbc06
莊理竣 <andy28572463@gmail.com>
2022-06-18 17:20:22 +0800

    [新增] 抽出Dialog共同部分放進Mixins,減少重複程式碼

--- src/components/CommandBook.vue
+++ src/components/CommandBook.vue
@@ -55,17 +55,10 @@
 
 <script>
 import { required } from '@vuelidate/validators';
-import { useVuelidate } from '@vuelidate/core';
+import commandDialogMixins from '@/mixins/CommandDialogMixins';
 
 export default {
-  props: ['isNew', 'isLoading', 'tempItem'],
-  data() {
-    return {
-      isVisible: false,
-      item: {},
-      v$: useVuelidate({ $lazy: true }),
-    };
-  },
+  mixins: [commandDialogMixins],
   validations() {
     return {
       item: {
@@ -74,34 +67,6 @@ export default {
         description: { required },
       },
     };
-  },
-  methods: {
-    show() {
-      this.isVisible = true;
-    },
-    close() {
-      this.isVisible = false;
-    },
-    async saveItem() {
-      // 設置$lazy: true 需使用非同步驗證
-      const isFormCorrect = await this.v$.$validate();
-      if (!isFormCorrect) return;
-      this.$emit('saveItem', this.item);
-      // 新增完將item重製可以再新增
-      if (this.isNew) {
-        this.item = {};
-        this.v$.$reset();
-      }
-    },
-  },
-  watch: {
-    isVisible: {
-      // eslint-disable-next-line no-unused-vars
-      handler(newVal, oldVal) {
-        this.v$.$reset();
-        this.item = JSON.parse(JSON.stringify(this.tempItem));
-      },
-    },
   },
 };
 </script>
